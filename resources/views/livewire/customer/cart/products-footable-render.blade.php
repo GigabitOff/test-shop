3037c68595fb043fe4b6c9813f60c0e67f68bf0c
GigabitOff <itsystems571@gmail.com>
2023-03-09 09:47:56 +0100

    SBGD-435 Mission accomplished - Add  ( SBGD-441 ) (finish)

--- resources/views/livewire/customer/cart/products-footable-render.blade.php
+++ resources/views/livewire/customer/cart/products-footable-render.blade.php
@@ -6,9 +6,9 @@
                 <label class="check js-select-all">
                     <input class="check__input" type="checkbox"
                            onchange="Livewire.emit('eventCheckAllChanged', this.checked)"
-                    @if($checkAll) checked @endif />
+                           @if($checkAll) checked @endif />
                     <span class="check__box"></span><span
-                            class="check__txt"></span></label>
+                        class="check__txt"></span></label>
                 <span>@lang('custom::site.Selected products')</span>
             </div>
         </th>
@@ -24,32 +24,32 @@
         <tr>
             <td>
                 <div class="d-flex align-items-center">
-                @if($cartProduct->availability)
-                    <label class="check">
-                        <input class="check__input" type="checkbox"
-                        onclick="document.cartProduct.setCheck('{{$cartProduct->cartUuid}}', this.checked)"
-                        @if($cartProduct->cartChecked) checked @endif/>
-                        <span class="check__box"></span>
-                        <span class="check__txt"></span>
-                    </label>
-                @else
-                    <label class="check"></label>
-                @endif
+                    @if($cartProduct->availability)
+                        <label class="check">
+                            <input class="check__input" type="checkbox"
+                                   onclick="document.cartProduct.setCheck('{{$cartProduct->cartUuid}}', this.checked)"
+                                   @if($cartProduct->cartChecked) checked @endif/>
+                            <span class="check__box"></span>
+                            <span class="check__txt"></span>
+                        </label>
+                    @else
+                        <label class="check"></label>
+                    @endif
                     <div class="table-product-card">
                         @if($cartProduct->availability)
-                        <div class="table-product-card__img">
-                            @if($cartProduct->images->isNotEmpty())
-                                @foreach($cartProduct->images as $key_image => $image)
-                                    @if($key_image == 0 )
-                                        <img src="{{\Storage::disk('public')->url($image->url)}}"
-                                             alt="{{$cartProduct->name}}">
-                                    @endif
-                                @endforeach
-                            @else
-                                <img src="{{fallbackProductImageUrl($cartProduct->imagefullUrl)}}"
-                                     alt="{{$cartProduct->name}}">
-                            @endif
-                        </div>
+                            <div class="table-product-card__img">
+                                @if($cartProduct->images->isNotEmpty())
+                                    @foreach($cartProduct->images as $key_image => $image)
+                                        @if($key_image == 0 )
+                                            <img src="{{\Storage::disk('public')->url($image->url)}}"
+                                                 alt="{{$cartProduct->name}}">
+                                        @endif
+                                    @endforeach
+                                @else
+                                    <img src="{{fallbackProductImageUrl($cartProduct->imagefullUrl)}}"
+                                         alt="{{$cartProduct->name}}">
+                                @endif
+                            </div>
                         @else
                             <div class="product-card__badges">
                                 <span class="product-card__label not">
@@ -57,15 +57,15 @@
                                 </span>
                             </div>
                             <div class="table-product-card__img">
-                            <img style="filter: grayscale(1);"
-                                 src="{{fallbackImageUrl('app.fallback_image.cart', '')}}"
-                                 alt="">
+                                <img style="filter: grayscale(1);"
+                                     src="{{fallbackImageUrl('app.fallback_image.cart', '')}}"
+                                     alt="">
                             </div>
                         @endif
                         <div class="table-product-card__desc"><span
-                                    class="table-product-card__art">№{{$cartProduct->articul}}</span><a
-                                    class="table-product-card__title"
-                                    href="{{route('products.show', $cartProduct->slug)}}">
+                                class="table-product-card__art">№{{$cartProduct->articul}}</span><a
+                                class="table-product-card__title"
+                                href="{{route('products.show', $cartProduct->slug)}}">
                                 @foreach ($cartProduct->translations as $item_d)
                                     @if($item_d->locale == app()->getLocale())
                                         {!!$item_d->name !!}
@@ -89,24 +89,31 @@
                     <span>{{$cartProduct->availabilityText}}</span>
                 </div>
             </td>
+            @php($productPriceField = App\Models\Product::getPriceField(null, $cartProduct->price_sale,  $cartProduct->price_wholesale))
             <td>
-                <span class="big">{!! formatNbsp(formatMoney($cartProduct->price - $cashbackUsed) . ' ' . __('custom::site.UAH')) !!}</span>
-                <span class="small">{!! formatNbsp(formatMoney($cartProduct->price_retail) . ' ' . __('custom::site.UAH')) !!}</span>
+                <span
+                    class="big">{!! formatNbsp(formatMoney($cartProduct->$productPriceField - $cashbackUsed) . ' ' . __('custom::site.UAH')) !!}</span>
+                <s style="text-decoration: line-through; color: grey; font-size: 17px;">{!! formatNbsp(formatMoney($cartProduct->price_rrc) . ' ₴') !!}</s>&nbsp;
+
             </td>
             <td class="text-center">
                 <div class="counter">
                     <div class="counter__btn minus"></div>
                     <div class="counter__field">
                         <input type="number" value="{{$cartProduct->cartQuantity}}" min="1"
-                        onchange="document.cartProduct.changeQuantity(this,
-                        '{{$cartProduct->cartUuid}}')"/>
+                               onchange="document.cartProduct.changeQuantity(this,
+            '{{$cartProduct->cartUuid}}')"/>
                     </div>
                     <div class="counter__btn plus"></div>
                 </div>
             </td>
             <td>
-                <span class="big">{!! formatNbsp(formatMoney($cartProduct->cartCost - $cashbackUsed) . ' ' . __('custom::site.UAH')) !!}</span>
-                <span class="small">{!! formatNbsp(formatMoney($cartProduct->totalPriceRetail) . ' ' . __('custom::site.UAH')) !!}</span>
+                <span
+                    class="big">{!! formatNbsp(formatMoney($cartProduct->$productPriceField * $cartProduct->cartQuantity - $cashbackUsed) . ' ' . __('custom::site.UAH')) !!}</span>
+                <s style="text-decoration: line-through; color: grey; font-size: 17px;">
+                    {!! formatNbsp(formatMoney($cartProduct->price_rrc * $cartProduct->cartQuantity) . ' ₴') !!}
+                </s>&nbsp;
+
             </td>
             <td class="text-end">
                 <button class="button-delete ico_trash" type="button"
