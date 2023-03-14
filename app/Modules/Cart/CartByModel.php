2ca217a83015d4e65dbd552fd2e8f85735551e28
GigabitOff <itsystems571@gmail.com>
2023-03-14 10:17:33 +0100

    SBGD - 446 1.1.7 - Basket sum all(Update).

--- app/Modules/Cart/CartByModel.php
+++ app/Modules/Cart/CartByModel.php
@@ -87,7 +87,12 @@ class CartByModel implements CartContract
 
     public function totalCartCheckedQuantity()
     {
-        return $this->checkedProducts()->map->cartQuantity->sum();
+        // Get all checked products
+        $checkedProducts = $this->checkedProducts();
+        // Get only unique products by "id" field
+        $uniqueProducts = $checkedProducts->unique('id');
+        // Return the count of unique products
+        return $uniqueProducts->count();
     }
 
     public function totalCartCheckedCost()
