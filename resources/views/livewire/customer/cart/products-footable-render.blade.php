9d201a238bfb03b2a1922f33acabbc1e8d1ed34a
GigabitOff <itsystems571@gmail.com>
2023-03-17 10:51:54 +0100

    SBGD - 483 Add-ons(Update ALL) (Finish)(Duble) .

--- resources/views/livewire/customer/cart/products-footable-render.blade.php
+++ resources/views/livewire/customer/cart/products-footable-render.blade.php
@@ -99,7 +99,7 @@
                         <?php $user = $user ?? auth()->user(); ?>
                         @if (is_object($user) && $user->is_founder != 0)
                             @if ($cartProduct->price_sale_show == 0 and $cartProduct->price_wholesale != 0)
-                                <span style="color: grey; font-size: 17px;"> {!! formatNbsp(formatMoney($cartProduct->price_rrc * $cartProduct->quantity) . ' ₴') !!} </span>
+                                <span style="color: grey; font-size: 17px;"> {!! formatNbsp(formatMoney($cartProduct->price_rrc) . ' ₴') !!} </span>
                             @else
                                 <s style="text-decoration: line-through; color: grey; font-size: 17px;"> {!! formatNbsp(formatMoney($cartProduct->price_rrc) . ' ₴') !!} </s>
                             @endif
@@ -135,7 +135,7 @@
                           <?php $user = $user ?? auth()->user(); ?>
                         @if (is_object($user) && $user->is_founder != 0)
                             @if ($cartProduct->price_sale_show == 0 and $cartProduct->price_wholesale != 0)
-                                <span style="color: grey; font-size: 17px;"> {!! formatNbsp(formatMoney($cartProduct->price_rrc) . ' ₴') !!} </span>
+                                <span style="color: grey; font-size: 17px;"> {!! formatNbsp(formatMoney($cartProduct->price_rrc * $cartProduct->cartQuantity) . ' ₴') !!} </span>
                             @else
                                 <s style="text-decoration: line-through; color: grey; font-size: 17px;"> {!! formatNbsp(formatMoney($cartProduct->price_rrc) . ' ₴') !!} </s>
                             @endif
