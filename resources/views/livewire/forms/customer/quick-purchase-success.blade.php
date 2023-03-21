134e88dbbecb8c3006934f853177746d1f0fc848
Vladimir Zabara <v.zabara@fairtech.group>
2023-03-21 13:32:02 +0200

    SBGD-466: add success popup styles

--- resources/views/livewire/forms/customer/quick-purchase-success.blade.php
+++ resources/views/livewire/forms/customer/quick-purchase-success.blade.php
@@ -1,11 +1,7 @@
 <div class="modal-content modal-success" id="modal-quick-purchase-content" data-mode="{{$status}}">
     <div class="modal-header">
-        <h5 class="modal-title">{{__('custom::site.message')}}<span>{{__('custom::site.on_project_domain')}}</span></h5>
-        <button class="close" type="button"
-                data-dismiss="modal"
-                aria-label="Close"><span
-                class="ico_close"></span>
-        </button>
+        <h5 class="modal-title">{{__('custom::site.message')}}<small>{{__('custom::site.on_project_domain')}}</small></h5>
+        <button class="btn-close" type="button" data-bs-dismiss="modal"></button>
     </div>
     <div class="modal-body">
         <div class="form-group">
@@ -16,7 +12,7 @@
         <div class="mt-5">
             <button class="button button-secondary button-block button-lg success-focusable"
                     onkeyup="if(event.keyCode===13){event.target.click()}"
-                    data-dismiss="modal"
+                    data-bs-dismiss="modal"
                     type="button">
                 {{__('custom::site.close')}}
             </button>
