5dd6a81fe1bf3f6bfe76cf04efc37ceb94a8c32d
Vladimir Zabara <v.zabara@fairtech.group>
2023-03-21 15:06:27 +0200

    SBGD-466: fix success message text

--- resources/views/livewire/forms/customer/quick-purchase-success.blade.php
+++ resources/views/livewire/forms/customer/quick-purchase-success.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-content modal-success" id="modal-quick-purchase-content" data-mode="{{$status}}">
     <div class="modal-header">
-        <h5 class="modal-title">{{__('custom::site.message')}}<small>{{__('custom::site.on_project_domain')}}</small></h5>
+        <h5 class="modal-title">{{__('custom::site.quick_purchase')}}<small>{{__('custom::site.on_project_domain')}}</small></h5>
         <button class="btn-close" type="button" data-bs-dismiss="modal"></button>
     </div>
     <div class="modal-body">
