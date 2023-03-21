e7a6c29257a7af9fbbabcd51d22689a0bef5515a
GigabitOff <itsystems571@gmail.com>
2023-03-21 12:04:24 +0100

    Test - Test bagfix.

--- resources/views/components/comparable-product-card.blade.php
+++ resources/views/components/comparable-product-card.blade.php
@@ -5,13 +5,36 @@
                 <div class="compare-item__label {{$product->availabilityCss}}">{{$product->availabilityText}}</div>
                 <div class="compare-item__action">
                     <div class="btn-delete"
-                         onclick="Livewire.emit('eventRemoveComparisonsItem', {'product_id' : {{$product->id}} })"><span class="ico_trash"></span></div>
+                         onclick="Livewire.emit('eventRemoveComparisonsItem', {'product_id' : {{$product->id}} })"><span class="ico_trash"></span>
+                    </div>
                 </div><img src="{{$product->mainImageUrl}}" alt="{{$product->name}}"/>
             </div>
             <div class="compare-item__info">
                 <div class="compare-item__title"><a href="{{route('products.show', $product->slug)}}">{{$product->name}}</a></div>
-                <div class="compare-item__price"><del>{!! formatNbsp(formatMoney($product->price_rrc) . ' ₴') !!}</del>
-                    {!! formatNbsp(formatMoney($product->price) . ' ₴') !!}</div>
+                {{--  Block for determining the type and type of prices in accordance with the type of user.--}}
+                @php($productPriceField = App\Models\Product::getPriceFieldWithParams(null, $product->price_sale,  $product->price_wholesale , $product->price_sale_show))
+                <div class="product-card__price">
+                    @if ($product->price_sale_show != 0 and $product->price_wholesale == 0 or $product->price_sale_show == 0 and $product->price_wholesale != 0 or $product->price_sale_show != 0 and $product->price_wholesale != 0)
+                        <span>
+                        <?php $user = $user ?? auth()->user(); ?>
+                            @if (is_object($user) && $user->is_founder != 0)
+                                <span style="color: #9f041b; font-size: 17px;"> {!! formatNbsp(formatMoney($product->price_rrc) . ' ₴') !!} </span>
+                            @else
+                                @if (!is_object($user) and $product->price_sale_show != 0 and $product->price_sale != 0)
+                                    <s style="text-decoration: line-through; color: #9f041b; font-size: 17px;"> {!! formatNbsp(formatMoney($product->price_rrc) . ' ₴') !!} </s>
+                                @else
+                                    <s style="text-decoration: line-through; color: #9f041b; font-size: 17px;"></s>
+                                @endif
+                                @if (is_object($user) and $product->price_sale_show != 0)
+                                    <s style="text-decoration: line-through; color: #9f041b; font-size: 17px;"> {!! formatNbsp(formatMoney($product->price_rrc) . ' ₴') !!} </s>
+                                @endif
+                            @endif
+                    </span>
+                    @elseif($product->price_wholesale == 0 and $product->price_sale_show == 0 )
+                        <span style="color: grey; font-size: 17px;"></span>
+                    @endif
+                    <span class="big">  {!! formatNbsp(formatMoney($product->{$productPriceField}) . ' ₴') !!}</span>
+                </div>
             </div>
         </div>
     </div>
@@ -20,7 +43,7 @@
             @foreach($attrs as $id => $name)
                 <li class="filtered attribute-{{$id}}"
                     data-attribute="{{$id}}"
-                    data-terms="{{$this->productAttributeIdsLine($product)}}">
+                    data-hash="{{$this->productAttributeValuesHash($product, $id)}}">
                     <div class="compare-item__item"><span
                             class="lbl">{{$name}}</span><span
                             class="value">{{$this->productAttributeValuesLine($product, $id)}}</span></div>
@@ -43,7 +66,7 @@
         </div>
         @if($product->can_be_sold)
             <a class="button-outline button-small"
-               onclick="Livewire.emit('eventCartAddProduct', {'product_id' : {{$product->id}}, 'show_notification':1, 'price_added' : {{$product->price}}, 'quantity': $(this).closest('.compare-item__footer').find('.counter input').get(0).value})" href="javascript:void(0);">
+               onclick="Livewire.emit('eventCartAddProduct',{'product_id':{{$product->id}},'show_notification':1,'price_added':parseFloat('{{$product->price}}'),'quantity':$(this).closest('.compare-item__footer').find('.counter input').get(0).value})" href="javascript:void(0);">
                 @lang('custom::site.Buy')
             </a>
         @else
