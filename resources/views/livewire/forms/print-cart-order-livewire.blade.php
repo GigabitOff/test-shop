ece8de3bc63f8e69d53a0d2724ec6381dacb7c11
GigabitOff <itsystems571@gmail.com>
2023-03-22 09:17:42 +0100

    Bagfix- print cart basket.

--- resources/views/livewire/forms/print-cart-order-livewire.blade.php
+++ resources/views/livewire/forms/print-cart-order-livewire.blade.php
@@ -1,4 +1,4 @@
-<div class="modal-content">
+<div class="modal-content" >
     <div class="modal-header">
         <h5 class="modal-title">@lang('custom::site.print_order')
             <small>@lang('custom::site.on_project_domain')</small>
@@ -8,9 +8,11 @@
     <div class="modal-body">
         <div class="lk-modal__sort">
             <div class="lk-page__sort-items">
+
                 <div class="filter-sort-item @if($printWithSale) is-active @endif">
                     <a class="filter-sort-item__link" href="#!" wire:click="printWithSale">
                         <span class="filter-sort-item__title">
+
                         @lang('custom::site.with_sale')</span>
                     </a></div>
                 <div class="filter-sort-item @if(!$printWithSale) is-active @endif">
@@ -41,40 +43,46 @@
                 </thead>
                 <tbody>
                 @foreach($products as $product)
-                <tr>
-                    <td class="footable-first-visible" style="display: table-cell;">
-                        <span>{{$product->articul}}</span>
-                    </td>
-                    <td style="display: table-cell;">
-                        <a class="big" href="{{route('products.show', $product->slug)}}">
-                        {{$product->name}}</a>
-                    </td>
-                    <td>
-                        <div class="status {{$product->availabilityCss}}">
-                            <span class="circle"></span>
-                            <span>{{$product->statusText}}</span>
-                        </div>
-                    </td>
-                    <td style="display: table-cell;">
-                        <span>{{$product->cartQuantity}}</span>
-                    </td>
-                    <td style="display: table-cell;">
-                        @if($printWithSale && $product->price_wholesale != 0)
-                            @if($product->price_sale_show != 0)
-                                {!! formatNbsp(formatMoney($product->price_sale * $product->cartQuantity) . ' ₴') !!}
-                            @else
-                                @if($product->price_wholesale > 0)
-                                    {!! formatNbsp(formatMoney($product->price_wholesale * $product->cartQuantity) . ' ₴') !!}
-                                @else
-                                    {!! formatNbsp(formatMoney($product->price_rrc * $product->cartQuantity) . ' ₴') !!}
-                                @endif
-                            @endif
-                        @else
-                            {!! formatNbsp(formatMoney($product->price_rrc * $product->cartQuantity) . ' ₴') !!}
-                        @endif
+                    <tr >
+                        <td class="footable-first-visible" style="display: table-cell;">
+                            <span>{{$product->articul}}</span>
+                        </td>
+                        <td style="display: table-cell;">
+                            <a class="big" href="{{route('products.show', $product->slug)}}">
+                                {{$product->name}}</a>
+                        </td>
+                        <td>
+                            <div class="status {{$product->availabilityCss}}">
+                                <span class="circle"></span>
+                                <span>{{$product->statusText}}</span>
+                            </div>
+                        </td>
 
-                    </td>
-                </tr>
+                        {{--<td>
+                            <span id="ms">{{$product->cartQuantity}}</span>
+                        </td>--}}
+                        <td>
+                            <span >{{$product->cartQuantity}}</span>
+                        </td>
+
+
+                        <td >
+                            @if($printWithSale && $product->price_wholesale != 0)
+                                @if($product->price_sale_show != 0)
+                                    {!! formatNbsp(formatMoney($product->price_sale * $product->cartQuantity) . ' ₴') !!}
+                                @else
+                                    @if($product->price_wholesale > 0)
+                                        {!! formatNbsp(formatMoney($product->price_wholesale * $product->cartQuantity) . ' ₴') !!}
+                                    @else
+                                        {!! formatNbsp(formatMoney($product->price_rrc * $product->cartQuantity) . ' ₴') !!}
+                                    @endif
+                                @endif
+                            @else
+                                {!! formatNbsp(formatMoney($product->price_rrc * $product->cartQuantity) . ' ₴') !!}
+                            @endif
+
+                        </td>
+                    </tr>
                 @endforeach
                 </tbody>
                 <tfoot></tfoot>
@@ -87,38 +95,38 @@
                     <dt>@lang('custom::site.total') ({{cart()->totalQuantity()}}
                         @lang('custom::site.products') )
                     </dt>
-                    <dd class="big">
-
+                    <dd  class="big">
                         {{$totalCost}}
                         @lang('custom::site.UAH')
                     </dd>
                 </dl>
                 <button class="button-accent button-accent" type="button"
-                    data-bs-dismiss="modal" onclick="window.printPopup();">
+                        data-bs-dismiss="modal" onclick="window.printPopup();">
                     @lang('custom::site.do_print')
                 </button>
             </div>
         </div>
     </div>
-<script>
-    window.printPopup = function () {
-        /**
-         * Для печати таблицы в полном формате в мобильной версии
-         * подменяем таблицу из holder-a
-         */
-        const $printable = $('#m-print-cart-order .modal-body').clone();
-        // const tableData = $('#mpo_footable-content').html();
 
-        //$('.lk-modal__sort').remove();
-        // $printable.prepend(tableData);
-        // $printable.find('#mpo_footable-holder').remove();
-        //$('.modal-body').remove();
+    <script>
+        window.printPopup = function () {
+            /**
+             * Для печати таблицы в полном формате в мобильной версии
+             * подменяем таблицу из holder-a
+             */
+            const $printable = $('#m-print-cart-order .modal-body').clone();
+            // const tableData = $('#mpo_footable-content').html();
 
-        const newWin = window.open('', 'Print-Window');
+            //$('.lk-modal__sort').remove();
+            // $printable.prepend(tableData);
+            // $printable.find('#mpo_footable-holder').remove();
+            //$('.modal-body').remove();
 
-        newWin.document.open();
+            const newWin = window.open('', 'Print-Window');
 
-        newWin.document.write(
+            newWin.document.open();
+
+            newWin.document.write(
                 '<html> ' +
                 '   <link rel="stylesheet" href="/assets/css/main.css">' +
                 '   <body onload="setTimeout(function(){window.print()},90)">' +
@@ -130,12 +138,25 @@
                 $printable.html() +
                 '   </body>' +
                 '</html>'
-        );
+            );
 
-        setTimeout(function () {
-            newWin.print();
-        }, 300);
-    }
-    //# sourceURL=print-order-livewire.js
-</script>
+
+        }
+        //# sourceURL=print-order-livewire.js
+    </script>
+
 </div>
+<script>
+    setInterval(function() {
+        $('#mst').load(location.href + ' #mst');
+    }, 1000);
+</script>
+
+<script>
+    var modal = document.getElementById('m-print-cart-order');
+    modal.addEventListener('show.bs.modal', function (event) {
+            var item = document.querySelector('.filter-sort-item');
+            item.querySelector('a').click();
+        }
+    );
+</script>
