50e8796a5ae33eb8854ac0ce9a03031f7c69b220
Anatoliy Pychev <tpychev@dmail.com>
2023-03-31 23:58:38 +0300

    SBGD-535: Fix exported data for /getFastOrders

--- app/Http/Resources/Api/V1/OrderResource.php
+++ app/Http/Resources/Api/V1/OrderResource.php
@@ -20,9 +20,9 @@ class OrderResource extends JsonResource
                 'id_site' => $this->id,
                 'total' => $this->total , // сумма заказа
                 'total_quantity' => $this->total_quantity, // количество товаров
-                'type_payment' => $this->type_payment, //тип оплаты, значения - "nal" или "beznal"
+//                'type_payment' => $this->type_payment, //тип оплаты, значения - "nal" или "beznal"
                 'date_registration' =>  $this->created_at, // дата создания заказа
-                'date_delivery' => $this->date_delivery ?? '', // плановая дата отгрузки из 1С
+//                'date_delivery' => $this->date_delivery ?? '', // плановая дата отгрузки из 1С
                 'status_id_1c' => $this->status->id_1c ?? '', // id статуса заказа
                 'order_products' => OrderProductResource::collection($this->products),
                 'updated_at' => $this->updated_at,
