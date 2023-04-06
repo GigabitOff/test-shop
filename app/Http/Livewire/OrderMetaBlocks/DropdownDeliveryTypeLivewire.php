17545993c829d5794168e0a7b01b22bb9b57880d
GigabitOff <itsystems571@gmail.com>
2023-04-06 12:58:32 +0200

    SBGD-486 Update bagfix v 0.7

--- app/Http/Livewire/OrderMetaBlocks/DropdownDeliveryTypeLivewire.php
+++ app/Http/Livewire/OrderMetaBlocks/DropdownDeliveryTypeLivewire.php
@@ -4,16 +4,6 @@ namespace App\Http\Livewire\OrderMetaBlocks;
 
 use App\Models\DeliveryType;
 use Livewire\Component;
-use App\Http\Livewire\Traits\WithFilterableDropdown;
-use App\Models\DeliveryAddress;
-use App\Models\Warehouse;
-
-use App\Models\City;
-use App\Models\Contract;
-use Illuminate\Database\Eloquent\Model;
-use App\Models\PaymentType;
-use App\Models\User;
-use Illuminate\Http\Request;
 
 class DropdownDeliveryTypeLivewire extends Component
 {
@@ -23,21 +13,16 @@ class DropdownDeliveryTypeLivewire extends Component
 
     public function mount()
     {
-
         $this->deliveryTypeList = $this->setDeliveryTypeList();
         $this->deliveryTypeName = $this->deliveryTypeList[$this->deliveryTypeId] ?? '';
     }
-
     public function render()
     {
-
     return view('livewire.order-meta-blocks.dropdown-delivery-type-livewire');
-
     }
 
     public function updatedDeliveryTypeId($id)
     {
-
         $this->deliveryTypeName = $this->deliveryTypeList[$id] ?? '';
         $this->emit('eventSetOrderDeliveryType', $id, $this->deliveryTypeName);
 
@@ -45,11 +30,8 @@ class DropdownDeliveryTypeLivewire extends Component
 
     private function setDeliveryTypeList(): array
     {
-
         return DeliveryType::query()
             ->withTranslation()->get()
             ->keyBy('id')->map->name->toArray();
     }
-
-
 }
