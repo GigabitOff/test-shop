fbf494a699523f1e6367ab543e98964a13b13708
Max <m.nykytiuk@fairtech.group>
2023-04-07 11:40:44 +0200

    SBGD-559: Сomplete the display of characteristics - fix2

--- resources/views/livewire/catalog/product/product-atributes-livewire.blade.php
+++ resources/views/livewire/catalog/product/product-atributes-livewire.blade.php
@@ -1,22 +1,14 @@
-@if($layout['columns']!=3)
-<div>
-@endif
-    <ul class="specification-list">
-        @foreach(array_slice($attributes, ceil($countAttributes/2)) as $attr)
-            <li>
-                @foreach($attr['attribute']['translations'] as $translation_name)
-                    @if($translation_name['locale'] == app()->getLocale())
-                        <span>{{$translation_name['name']}}</span>
-                    @endif
-                @endforeach
-                @foreach($attr['translations'] as $translation_value)
-                    @if($translation_value['locale'] == app()->getLocale())
-                        <strong>{{$translation_value['name']}}</strong>
-                    @endif
-                @endforeach
-            </li>
+@foreach (array_slice($attributes, ceil($countAttributes / 2)) as $attr)
+    <li>
+        @foreach ($attr['attribute']['translations'] as $translation_name)
+            @if ($translation_name['locale'] == app()->getLocale())
+                <span>{{ $translation_name['name'] }}</span>
+            @endif
         @endforeach
-    </ul>
-@if($layout['columns']!=3)
-</div>
-@endif
+        @foreach ($attr['translations'] as $translation_value)
+            @if ($translation_value['locale'] == app()->getLocale())
+                <strong>{{ $translation_value['name'] }}</strong>
+            @endif
+        @endforeach
+    </li>
+@endforeach
--- resources/views/livewire/catalog/product/product-specification-livewire.blade.php
+++ resources/views/livewire/catalog/product/product-specification-livewire.blade.php
@@ -1,50 +1,42 @@
 @php $attributes = $data->attributeValues->toArray();@endphp
-@if($attributes)
-<div class="@if($layout['columns']!=3) product-full-box --specification @else product-full-box --specification-single @endif">
-    <div class="product-full-box__head">
-        <div class="product-full-box__title" id="specification">@lang('custom::site.Specification')</div>
-    </div>
-    <div class="product-full-box__body --overflow">
-        <div>
-            @php
-            //add size and weight to attribute
-            if($data->weight)
-            array_push($attributes,
-            [ 'translations' => [ 0 => ["locale" => app()->getLocale(), "name" => $data->weight]],
-            'attribute' => ['translations' => [ 0 => [ "locale" => app()->getLocale(), "name" => \Lang::get('custom::site.Weight')]]]
-            ]);
-            if($data->depth||$data->width||$data->height)
-            array_push($attributes,
-            [ 'translations' => [0 => ["locale"=>app()->getLocale(), "name"=>$data->depth."х".$data->width."х".$data->height]],
-            'attribute' => ['translations' => [0 => [ "locale" => app()->getLocale(), "name" => \Lang::get('custom::site.Size')]]]
-            ]);
-            $countAttributes = count($attributes);
-            @endphp
+@if ($attributes)
+    <div
+        class="@if ($layout['columns'] != 3) product-full-box --specification @else product-full-box --specification-single @endif">
+        <div class="product-full-box__head">
+            <div class="product-full-box__title" id="specification">@lang('custom::site.Specification')</div>
+        </div>
+        <div class="product-full-box__body --overflow">
             <div>
-                <ul class="specification-list">
-                    @foreach(array_slice($attributes, 0, ceil($countAttributes/2)) as $attr)
-                        <li>
-                            @foreach($attr['attribute']['translations'] as $translation_name)
-                                @if($translation_name['locale'] == app()->getLocale())
-                                    <span>{{$translation_name['name']}}</span>
-                                @endif
-                            @endforeach
-                            @foreach($attr['translations'] as $translation_value)
-                                @if($translation_value['locale'] == app()->getLocale())
-                                    <strong>{{$translation_value['name']}}</strong>
-                                @endif
-                            @endforeach
-                        </li>
-                    @endforeach
-                </ul>
-                @if($layout['columns']==3)
-                    @include('livewire.catalog.product.product-atributes-livewire')
-                @endif
+                @php
+                    //add size and weight to attribute
+                    if ($data->weight) {
+                        array_push($attributes, ['translations' => [0 => ['locale' => app()->getLocale(), 'name' => $data->weight]], 'attribute' => ['translations' => [0 => ['locale' => app()->getLocale(), 'name' => \Lang::get('custom::site.Weight')]]]]);
+                    }
+                    if ($data->depth || $data->width || $data->height) {
+                        array_push($attributes, ['translations' => [0 => ['locale' => app()->getLocale(), 'name' => $data->depth . 'х' . $data->width . 'х' . $data->height]], 'attribute' => ['translations' => [0 => ['locale' => app()->getLocale(), 'name' => \Lang::get('custom::site.Size')]]]]);
+                    }
+                    $countAttributes = count($attributes);
+                @endphp
+                <div>
+                    <ul class="specification-list">
+                        @foreach (array_slice($attributes, 0, ceil($countAttributes / 2)) as $attr)
+                            <li>
+                                @foreach ($attr['attribute']['translations'] as $translation_name)
+                                    @if ($translation_name['locale'] == app()->getLocale())
+                                        <span>{{ $translation_name['name'] }}</span>
+                                    @endif
+                                @endforeach
+                                @foreach ($attr['translations'] as $translation_value)
+                                    @if ($translation_value['locale'] == app()->getLocale())
+                                        <strong>{{ $translation_value['name'] }}</strong>
+                                    @endif
+                                @endforeach
+                            </li>
+                        @endforeach
+                        @include('livewire.catalog.product.product-atributes-livewire')
+                    </ul>
+                </div>
             </div>
-            @if($layout['columns']!=3)
-                @include('livewire.catalog.product.product-atributes-livewire')
-            @endif
         </div>
     </div>
-</div>
 @endif
